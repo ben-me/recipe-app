12a3ee15820f9e5c191d12c62483a3b9cb63792f
Ben Me <benedikt.mehler@mailbox.org>
2022-09-06 12:30:19 +0200

    add veggie and dessert page, clean up index

---
+++ pages/dessert.js
@@ -0,0 +1,11 @@
+export default function dessert() {
+  return (
+    <>
+      <h1>Desserts</h1>
+      <p>
+        A desert is part of a meal that follows the main course. It is more
+        likely to be sweet and gets served in smaller portions.
+      </p>
+    </>
+  );
+}
---
+++ pages/veggie.js
@@ -0,0 +1,12 @@
+export default function veggie() {
+  return (
+    <>
+      <h1>Vegetables</h1>
+      <p>
+        Vegetables are green and healthy. Most kids do not like to eat them at
+        first but can quickly adapt. They are part of a balanced and healthy
+        nutrition.
+      </p>
+    </>
+  );
+}
